83d19b172b8d5e742ae97c640174f29c9fb32572
Codie (蒂诺) <5020407+lasso-codie@users.noreply.github.com>
2019-09-29 10:13:45 +0200

    Update base_class.cpp

--- example1_single-inheritance/my_module/my_module_cpp/base_class.cpp
+++ example1_single-inheritance/my_module/my_module_cpp/base_class.cpp
@@ -2,8 +2,8 @@
 /* DEALLOC */
 static void
 MyPy_BaseClass_dealloc(MyPy_BaseClass* self){
-   // Nothing to deallocate. Use this in case the base
-   // struct contains a pointer or so.
+   // deallocate class members and class itself
+   Py_TYPE(self)->tp_free((PyObject*)self);
 }
 
 /* NEW */
